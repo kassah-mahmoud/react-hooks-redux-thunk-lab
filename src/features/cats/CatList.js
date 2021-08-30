203599e85b5d9126fc22bdc757201e684420daef
Mahmoud <kassah.webdev@gmail.com>
2021-08-30 11:43:01 +0300

    Done.

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,14 @@
-// write your CatList component here
+import React from 'react'
+
+const CatList = ({catPics}) => {
+    return (
+        <div style={{
+            display: 'grid',
+            gridTemplateColumns: 3
+        }}>
+            {catPics?.map?.(pic => <img key={pic.id} src={pic.url} alt = "cat" />)}
+        </div>
+    )
+}
+
+export default CatList
